fbf0cd97165a296e0cbbb0887c29a5005a7ed9a0
Christian Heel <66922325+heelc29@users.noreply.github.com>
2024-09-03 18:31:56 +0200

    indentation_type

--- Tests/RegExTest.php
+++ Tests/RegExTest.php
@@ -16,183 +16,183 @@ use PHPUnit\Framework\TestCase;
  */
 class RegExTest extends TestCase
 {
-	public function testExample3()
-	{
-		$scheme = RegEx::optional('http://');
-		$host   = RegEx::capture('[^/]+', 'host');
+    public function testExample3()
+    {
+        $scheme = RegEx::optional('http://');
+        $host   = RegEx::capture('[^/]+', 'host');
 
-		$matches = RegEx::match('~^' . $scheme . $host . '~', 'http://www.php.net/index.html');
+        $matches = RegEx::match('~^' . $scheme . $host . '~', 'http://www.php.net/index.html');
 
-		$host = $matches['host'];
+        $host = $matches['host'];
 
-		$segment = '[^.]+';
+        $segment = '[^.]+';
 
-		$domain  = RegEx::capture($segment . '\.' . $segment . '$', 'domain');
-		$matches = RegEx::match('~' . $domain . '~', $host);
+        $domain  = RegEx::capture($segment . '\.' . $segment . '$', 'domain');
+        $matches = RegEx::match('~' . $domain . '~', $host);
 
-		self::assertEquals('php.net', $matches['domain']);
-	}
+        self::assertEquals('php.net', $matches['domain']);
+    }
 
-	public function testExample4()
-	{
-		$str = 'foobar: 2008';
+    public function testExample4()
+    {
+        $str = 'foobar: 2008';
 
-		$name  = RegEx::capture('\w+', 'name');
-		$digit = RegEx::capture('\d+', 'digit');
+        $name  = RegEx::capture('\w+', 'name');
+        $digit = RegEx::capture('\d+', 'digit');
 
-		$matches = RegEx::match('~' . $name . ': ' . $digit . '~', $str);
+        $matches = RegEx::match('~' . $name . ': ' . $digit . '~', $str);
 
-		self::assertEquals(
-			array(
-				'name'  => 'foobar',
-				'digit' => '2008'
-			),
-			$matches
-		);
-	}
+        self::assertEquals(
+            array(
+                'name'  => 'foobar',
+                'digit' => '2008'
+            ),
+            $matches
+        );
+    }
 
-	public function testOptional()
-	{
-		$regex = 'a' . RegEx::optional('b') . 'c';
+    public function testOptional()
+    {
+        $regex = 'a' . RegEx::optional('b') . 'c';
 
-		self::assertEquals(
-			array('result' => 'ac'),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaacccc'
-			)
-		);
+        self::assertEquals(
+            array('result' => 'ac'),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaacccc'
+            )
+        );
 
-		self::assertEquals(
-			array('result' => 'abc'),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaabcccc'
-			)
-		);
+        self::assertEquals(
+            array('result' => 'abc'),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaabcccc'
+            )
+        );
 
-		self::assertEquals(
-			array(),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaabbcccc'
-			)
-		);
-	}
+        self::assertEquals(
+            array(),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaabbcccc'
+            )
+        );
+    }
 
-	public function testOneOrMore()
-	{
-		$regex = 'a' . RegEx::oneOrMore('b') . 'c';
+    public function testOneOrMore()
+    {
+        $regex = 'a' . RegEx::oneOrMore('b') . 'c';
 
-		self::assertEquals(
-			array(),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaacccc'
-			)
-		);
+        self::assertEquals(
+            array(),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaacccc'
+            )
+        );
 
-		self::assertEquals(
-			array('result' => 'abc'),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaabcccc'
-			)
-		);
+        self::assertEquals(
+            array('result' => 'abc'),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaabcccc'
+            )
+        );
 
-		self::assertEquals(
-			array('result' => 'abbc'),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaabbcccc'
-			)
-		);
-	}
+        self::assertEquals(
+            array('result' => 'abbc'),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaabbcccc'
+            )
+        );
+    }
 
-	public function testNoneOrMore()
-	{
-		$regex = 'a' . RegEx::noneOrMore('b') . 'c';
+    public function testNoneOrMore()
+    {
+        $regex = 'a' . RegEx::noneOrMore('b') . 'c';
 
-		self::assertEquals(
-			array('result' => 'ac'),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaacccc'
-			)
-		);
+        self::assertEquals(
+            array('result' => 'ac'),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaacccc'
+            )
+        );
 
-		self::assertEquals(
-			array('result' => 'abc'),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaabcccc'
-			)
-		);
+        self::assertEquals(
+            array('result' => 'abc'),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaabcccc'
+            )
+        );
 
-		self::assertEquals(
-			array('result' => 'abbc'),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaabbcccc'
-			)
-		);
-	}
+        self::assertEquals(
+            array('result' => 'abbc'),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaabbcccc'
+            )
+        );
+    }
 
-	public function testAnyOfList()
-	{
-		$regex = 'a' . RegEx::anyOf('1', '2', '3') . 'c';
+    public function testAnyOfList()
+    {
+        $regex = 'a' . RegEx::anyOf('1', '2', '3') . 'c';
 
-		self::assertEquals(
-			array(),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaacccc'
-			)
-		);
+        self::assertEquals(
+            array(),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaacccc'
+            )
+        );
 
-		self::assertEquals(
-			array('result' => 'a1c'),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaa1cccc'
-			)
-		);
+        self::assertEquals(
+            array('result' => 'a1c'),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaa1cccc'
+            )
+        );
 
-		self::assertEquals(
-			array('result' => 'a2c'),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaa2cccc'
-			)
-		);
-	}
+        self::assertEquals(
+            array('result' => 'a2c'),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaa2cccc'
+            )
+        );
+    }
 
-	public function testAnyOfArray()
-	{
-		$regex = 'a' . RegEx::anyOf(array('1', '2', '3')) . 'c';
+    public function testAnyOfArray()
+    {
+        $regex = 'a' . RegEx::anyOf(array('1', '2', '3')) . 'c';
 
-		self::assertEquals(
-			array(),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaacccc'
-			)
-		);
+        self::assertEquals(
+            array(),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaacccc'
+            )
+        );
 
-		self::assertEquals(
-			array('result' => 'a1c'),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaa1cccc'
-			)
-		);
+        self::assertEquals(
+            array('result' => 'a1c'),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaa1cccc'
+            )
+        );
 
-		self::assertEquals(
-			array('result' => 'a2c'),
-			RegEx::match(
-				'~' . RegEx::capture($regex, 'result') . '~',
-				'aaaa2cccc'
-			)
-		);
-	}
+        self::assertEquals(
+            array('result' => 'a2c'),
+            RegEx::match(
+                '~' . RegEx::capture($regex, 'result') . '~',
+                'aaaa2cccc'
+            )
+        );
+    }
 }
